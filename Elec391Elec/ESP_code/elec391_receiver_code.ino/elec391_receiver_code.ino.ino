08541368ada50990318f190444331a923f623955
Nathan Briggs <briggs_nathan@hotmail.com>
2024-04-05 03:02:19 -0700

    sender and receiver code updates
    Altered to work with ESP32, 1 motor

--- Elec391Elec/ESP_code/elec391_receiver_code.ino/elec391_receiver_code.ino.ino
+++ Elec391Elec/ESP_code/elec391_receiver_code.ino/elec391_receiver_code.ino.ino
@@ -1,6 +1,7 @@
 #include <esp_now.h>
 #include <WiFi.h>
-
+#include "esp32/rom/ets_sys.h"
+#include "Arduino.h"
 
 #define PWM_M1 21  //M1 PWM pin, GPIO21/D21
 #define CW_M1  22  //M1 Clockwise rotation pin, GPIO22/D22
@@ -9,188 +10,91 @@
 #define PWM_M2 25  //M2 PWM pin, GPIO25/D25
 #define CW_M2  26 //M2 Clockwise rotation pin, GPIO26/D26
 #define CCW_M2 27 //M2 Counterclockwise rotation pin, GPIO27/D27
+#define LED 18
+
+portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
+
+int target_M1 = 120;
+
+int temp_encoder_M1 = 0;
+int pos_M1 = 0;
+
+double controlSignal_M1 = 0.0;
+volatile long previousTime = 0;
+volatile long ePrevious_M1 = 0;
+volatile double eIntegral_M1 = 0.0;
 
 hw_timer_t *Timer0_Cfg = NULL;
 
-long encoder_M1 = 0;
-long temp_encoder_M1 = 0;
-long encoder_M2 = 0;
+void IRAM_ATTR Timer0_ISR(){
+  portENTER_CRITICAL_ISR(&mux);
+    pos_M1 = temp_encoder_M1/134;
+  portEXIT_CRITICAL_ISR(&mux); 
 
-volatile long pos_M1 = 0;
-volatile long pos_M2 = 0;
-
-//variables for testing purposes
-int target_M1 = 120;
-int target_M2 = 90;
-//volatile int ISR_time = 0;
-
-//PID
- float u_M1 = 0;
- float u_M2 = 0;
-
-float kp_M1 = 10;   //60
-float ki_M1 = 0.01; //600
-float kd_M1 = -0.1;  //0.1
-
-float kp_M2 = 10;   //60
-float ki_M2 = 0.01; //600
-float kd_M2 = 0.1;  //0.1
-
-
-volatile long previousTime = 0;
-volatile float ePrevious_M1 = 0;
-volatile float ePrevious_M2 = 0;
-volatile float eIntegral_M1 = 0;
-volatile float eIntegral_M2 = 0;
-volatile float eDerivativeBuffer_M1[10] = {0.0};
-volatile float eDerivativeBuffer_M2[10] = {0.0};
-const int eDerivativeSamples = 10;
-
-void IRAM_ATTR Timer0_ISR()
-{
-  encoder_M1 = temp_encoder_M1;
-  //int start = micros();
-  //convert encoder pulses to degrees, 136 pulses per degree
-  pos_M1 = encoder_M1/102;
-  //pos_M2 = encoder_M2/102;
-
- 
-
-  //PID control signal calculation
-  //pidController(target_M1, target_M2, pos_M1, pos_M2, kp_M1, kp_M2, ki_M1, ki_M2, kd_M1, kd_M2, u_M1, u_M2);
-
-  //Convert control signal into associated PWM value and direction, and send to motor
-  //moveMotor(CW_M1, CCW_M1, PWM_M1, -250);
-  //moveMotor(CW_M2, CCW_M2, PWM_M2, u_M2);
-
-  //ISR_time = micros() - start;
-   
-  //Serial.print(ISR_time);
-  
+  pidController(pos_M1, &controlSignal_M1);
+  moveMotor(CW_M1, CCW_M1, PWM_M1, controlSignal_M1);
+  //moveMotor(CW_M2, CCW_M2, PWM_M2, controlSignal_M2);
 }
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
-  
   
   pinMode(PWM_M1,OUTPUT);
   pinMode(CW_M1,OUTPUT);
   pinMode(CCW_M1,OUTPUT);
 
-  
- 
-  Timer0_Cfg = timerBegin(0, 80, true);
-  timerAttachInterrupt(Timer0_Cfg, &Timer0_ISR, true);
-  timerAlarmWrite(Timer0_Cfg, 1500, true);
-  timerAlarmEnable(Timer0_Cfg);
-
-   
-
-
+  pinMode(LED, OUTPUT);
 
   if (esp_now_init() != ESP_OK) {
-  Serial.println("Error initializing ESP-NOW");
-  return;
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  } 
 
-  
+  //register to receive package
+  esp_now_register_recv_cb(OnDataRecv);
+
+  //initialize timer, 1000hz/1ms
+  Timer0_Cfg = timerBegin(0, 80, true);
+  timerAttachInterrupt(Timer0_Cfg, &Timer0_ISR, true);
+  timerAlarmWrite(Timer0_Cfg, 10000, true);
+  timerAlarmEnable(Timer0_Cfg);
 }
-
- esp_now_register_recv_cb(OnDataRecv);
-
-}
-
 
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
-memcpy( &temp_encoder_M1, incomingData, sizeof(temp_encoder_M1));
- //Serial.print("Encoder_M1:");
-  //Serial.println(encoder_M1);
+  memcpy( &temp_encoder_M1, incomingData, sizeof(temp_encoder_M1));
+  //Serial.print("temp_encoder_M1:");
+  //Serial.println(temp_encoder_M1);
 }
 
+void pidController(int pos_M1, volatile double *u_M1){
+  //PID gain values
+  double kp_M1 = 3.0;
+  double ki_M1 = 0.5; //0.01;  
+  double kd_M1 = 0.1;
 
-void loop() {
- 
- pidController(target_M1, target_M2, pos_M1, pos_M2, kp_M1, kp_M2, ki_M1, ki_M2, kd_M1, kd_M2, u_M1, u_M2);
-
-  moveMotor(CW_M1, CCW_M1, PWM_M1, u_M1);
- 
- //erial.println("us");
-  Serial.print("target_M1:");
-  Serial.print(target_M1);
-  Serial.print(" | ");
-  Serial.print("Encoder_M1:");
-  Serial.print(encoder_M1);
-  Serial.print(" | ");
-  Serial.print("Pos_M1:");
-  Serial.print(pos_M1);
-  Serial.print("u_m1:");
-  Serial.print(e_M1);
-  Serial.println(" ");
-
-}
-
-
-
-void pidController(int target_M1, int target_M2, int pos_M1,  int pos_M2, float kp_M1, float kp_M2, float ki_M1, float ki_M2, float kd_M1, float kd_M2, float u_M1, float u_M2){
-  //time difference
+  //Time difference
   long currentTime = micros();
-  float deltaT = ((float)(currentTime-previousTime))/1.0e6;
+  double deltaT = ((double)(currentTime-(previousTime)))/1.0e6;
 
-  //compute error for proportional control
-  long e_M1 = pos_M1 - target_M1;
-  long e_M2 = pos_M2 - target_M2;
+  //Compute error for proportional control
+  double e_M1 = (pos_M1 - target_M1);
 
-  //compute integral error
+  //Compute integral term
   eIntegral_M1 = eIntegral_M1 + e_M1*deltaT;
-  eIntegral_M2 = eIntegral_M2 + e_M2*deltaT;
 
-  //compute derivative term using weighted sum filters
-  //shift the values in eDerivativeBuffer down and update the latest value to the last index
-  for (int i = 0; i<eDerivativeSamples-1; i++){
-    eDerivativeBuffer_M1[i] = eDerivativeBuffer_M1[i+1];
-  }
-  eDerivativeBuffer_M1[eDerivativeSamples-1] = (e_M1-ePrevious_M1)/(deltaT);
-
-  for (int i = 0; i<eDerivativeSamples-1; i++){
-    eDerivativeBuffer_M2[i] = eDerivativeBuffer_M2[i+1];
-  }
-  eDerivativeBuffer_M2[eDerivativeSamples-1] = (e_M2-ePrevious_M2)/(deltaT);
-
-  //apply weights to eDerivative terms
-  float eDerivative_M1 = 0.0;
-  float eDerivative_M2 = 0.0;
-  float weight_M1 = 1.0;
-  float weight_M2 = 1.0;
-  float weightSum = 0.0;
-  
-  for (int i=0; i<eDerivativeSamples; i++){
-    eDerivative_M1 += eDerivativeBuffer_M1[i]*weight_M1;
-    weightSum += weight_M1;
-    weight_M1 *= 1.2;
-  }
-
-  for (int i=0; i<eDerivativeSamples; i++){
-    eDerivative_M2 += eDerivativeBuffer_M2[i]*weight_M2;
-    weight_M2 *= 1.2;
-  }
-
-  //normalize eDerivative
-  eDerivative_M1 /= weightSum;
-  eDerivative_M2 /= weightSum;
-
-  //output PID control signal
-  u_M1 = (kp_M1*e_M1) + (ki_M1*eIntegral_M1) + (kd_M1*eDerivative_M1);
-  u_M2 = (kp_M2*e_M2) + (ki_M2*eIntegral_M2) + (kd_M2*eDerivative_M2);
+  //Compute derivative term
+  double eDerivative_M1 = (e_M1-(ePrevious_M1))/(deltaT);
 
   //update variables for next iteration
-  ePrevious_M1 = e_M1;
-  ePrevious_M2 = e_M2;
   previousTime = currentTime;
+  ePrevious_M1 = e_M1;
+
+  *u_M1 = (kp_M1*e_M1) + (ki_M1*eIntegral_M1) + (kd_M1*eDerivative_M1);
 }
 
-void moveMotor(int in1, int in2, int pwm_pin, float u){
-  float speed = fabs(u);  //motor power                              
+void moveMotor(int in1, int in2, int pwm_pin, double u){
+  double speed = fabs(u);  //motor power                              
   if(speed > 255){
     speed = 255;
   }
@@ -216,3 +120,14 @@ void moveMotor(int in1, int in2, int pwm_pin, float u){
   }
 }
 
+void loop() {
+  Serial.print("target_M1:");
+  Serial.print(target_M1);
+  Serial.print(" | ");
+  Serial.print("pos_M1:");
+  Serial.print(pos_M1);
+  Serial.print(" | ");
+  Serial.print("controlSignal_M1:");
+  Serial.println(controlSignal_M1);
+
+}
--- Elec391Elec/ESP_code/elec391_sender_code/elec391_sender_code.ino
+++ Elec391Elec/ESP_code/elec391_sender_code/elec391_sender_code.ino
@@ -1,41 +1,58 @@
-//white = B
-//yellow = A
 #include <esp_now.h>
 #include <WiFi.h>
 
-
 #define ENCA_M1 16 //YELLOW, M1 (Yaw) encoder intterupt pin, GPIO16/RX2
 #define ENCB_M1 18 //WHITE, M1 other encoder pin, GPIO18/D18
+#define readA_M1 bitRead(GPIO.in,16)  //faster than digitalRead
+#define readB_M1 bitRead(GPIO.in,18)  //faster than digitalRead
 
-
+//broadcast to receiver MAC address
 uint8_t broadcastAddress[] = {0x40, 0x91, 0x51, 0xFD, 0x26, 0x94};
-
-volatile long encoder_M1 = 0;
-
 
 esp_now_peer_info_t peerInfo;
 
+volatile long encoder_M1 = 0;
+
+void IRAM_ATTR readEncoderA_M1(){
+  if(readB_M1 != readA_M1){
+    encoder_M1++; //clockwise rotation
+  }
+  else{
+    encoder_M1--; //counter clockwise rotation
+  }
+}
+
+void IRAM_ATTR readEncoderB_M1(){
+  if(readA_M1 == readB_M1){
+    encoder_M1++; //clockwise rotation
+  }
+  else{
+    encoder_M1--; //counter clockwise rotation
+  }
+}
+
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
- // Serial.print("\r\nLast Packet Send Status:\t");
+  //Serial.print("\r\nLast Packet Send Status:\t");
   //Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
-
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
+
   WiFi.mode(WIFI_STA);
+
   if (esp_now_init() != ESP_OK) {
   Serial.println("Error initializing ESP-NOW");
   return;
   }
+
   pinMode(ENCA_M1,INPUT);
   pinMode(ENCB_M1,INPUT);
   
-
+  //register to get the status of the transmitted package
   esp_now_register_send_cb(OnDataSent);
 
-    //Register peer
+  //Register peer
   memcpy(peerInfo.peer_addr, broadcastAddress, 6);
   peerInfo.channel = 0;
   peerInfo.encrypt = false;
@@ -46,8 +63,8 @@ void setup() {
     return;
   }
 
-  attachInterrupt(digitalPinToInterrupt(ENCA_M1),readEncoderM1,RISING);
-
+  attachInterrupt(digitalPinToInterrupt(ENCA_M1), readEncoderA_M1, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENCB_M1), readEncoderB_M1, CHANGE);
 }
 
 void loop() {
@@ -55,23 +72,19 @@ void loop() {
 
   esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &encoder_M1, sizeof(encoder_M1));
 
- /*if (result == ESP_OK) {
+  /*if (result == ESP_OK) {
   Serial.println("Sent with success");
-}
-else {
+  }
+  else {
   Serial.println("Error sending the data");
-}*/
-Serial.println(encoder_M1);
+  }*/
 
-}
+  //Serial.print("ENCA_M1:");
+  //Serial.println(digitalRead(ENCA_M1));
+  //Serial.print("ENCB_M1:");
+  //Serial.println(digitalRead(ENCB_M1));
 
+  //Serial.print("encoder_M1: ");
+  //Serial.println(encoder_M1/134.0);
 
-
-void readEncoderM1(){
-  if(digitalRead(ENCB_M1) > 0){
-    encoder_M1++; //counter clockwise rotation
-  }
-  else{
-    encoder_M1--; //clockwise rotation
-  }
 }
