623cf26cbbe4fbeb6de9a034742c254d109b1fc7
Nathan Briggs <briggs_nathan@hotmail.com>
2024-04-07 19:25:50 -0700

    State Machine
    State machine added

--- Elec391Elec/ESP_code/elec391_receiver_code.ino/elec391_receiver_code.ino.ino
+++ Elec391Elec/ESP_code/elec391_receiver_code.ino/elec391_receiver_code.ino.ino
@@ -1,5 +1,6 @@
 #include <esp_now.h>
 #include <WiFi.h>
+#include <iostream>
 #include "esp32/rom/ets_sys.h"
 #include "Arduino.h"
 
@@ -11,7 +12,11 @@
 #define CW_M2  13 //M2 Clockwise rotation pin, GPIO13
 #define CCW_M2 12 //M2 Counterclockwise rotation pin, GPIO12
 
-//volatile int end_time = 0;
+//volatile long end_time = 0;
+
+enum states{HOME, DRAW_1, DRAW_2, DRAW_3};
+
+int state = HOME;
 
 typedef struct struct_message{
   int encoder_M1;
@@ -20,13 +25,13 @@ typedef struct struct_message{
 
 struct_message encoderData;
 
-int target_M1 = 120;
-int target_M2 = 360;
+int target_M1 = 0.0;
+int target_M2 = 0.0;
 
 volatile int pos_M1 = 0;
 volatile int pos_M2 = 0;
 
-volatile long prevTime = 0;
+//volatile long prevTime = 0;
 volatile double ePrev_M1 = 0;
 volatile double ePrev_M2 = 0;
 volatile double integral_M1 = 0.0;
@@ -39,7 +44,7 @@ double controlSignal_M2 = 0.0;
 hw_timer_t *Timer0_Cfg = NULL;
 
 void IRAM_ATTR Timer0_ISR(){
-  //int start = micros();
+  //long start = micros();
 
   //update motor position in degrees 1:134
   pos_M1 = encoderData.encoder_M1/134;
@@ -51,7 +56,7 @@ void IRAM_ATTR Timer0_ISR(){
   //send control signal to motors
   moveMotor(CW_M1, CCW_M1, PWM_M1, controlSignal_M1);
   moveMotor(CW_M2, CCW_M2, PWM_M2, controlSignal_M2);
-
+  
   //end_time = micros() - start;
 }
 
@@ -78,8 +83,9 @@ void setup() {
   //initialize timer, 130us period (80us ISR runtime): 61.5% duty cycle
   Timer0_Cfg = timerBegin(0, 80, true);
   timerAttachInterrupt(Timer0_Cfg, &Timer0_ISR, true);
-  timerAlarmWrite(Timer0_Cfg, 1500, true);
+  timerAlarmWrite(Timer0_Cfg, 1100, true);
   timerAlarmEnable(Timer0_Cfg);
+  //1/8000 or 8/1000
 }
 
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
@@ -95,20 +101,21 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
 }
 
 void pidController(int pos_M1, int pos_M2, volatile double *u_M1, volatile double *u_M2){
+  //sampling time of discrete controller
+  double T = 0.0011;
+
   //PID gain values
-  double T = 0.0015;  //sampling time of discrete controller
+  double kp_M1 = 2.5;   //2.5
+  double ki_M1 = 0.4;   //0.2
+  double kd_M1 = 0.0;   //-0.1
 
-  double kp_M1 = 2.0;
-  double ki_M1 = 0.2;
-  double kd_M1 = 0.1;
-
-  double kp_M2 = 2.0;
-  double ki_M2 = 0.2;
-  double kd_M2 = 0.1;
+  double kp_M2 = 2.5;   //2.5
+  double ki_M2 = 0.4;   //0.5
+  double kd_M2 = 0.0;   //-0.1
 
   //Time difference
-  long currentTime = micros();
-  double deltaT = ((double)(currentTime-(prevTime)))/1.0e6;
+  //long currentTime = micros();
+  //double deltaT = ((double)(currentTime-(prevTime)))/1.0e6;
 
   //Compute error for proportional control
   double e_M1 = (pos_M1 - target_M1);
@@ -118,44 +125,23 @@ void pidController(int pos_M1, int pos_M2, volatile double *u_M1, volatile doubl
   double proportional_M1 = e_M1*kp_M1;
   double proportional_M2 = e_M2*kp_M2;
 
-  //Compute integral term
-  //eIntegral_M1 = eIntegral_M1 + e_M1*deltaT;
+  //Compute integral term, reset integral term if motor position moves from above to below from target or vice versa, or if the target position = motor position
   double integral_M1 = (ki_M1*T*0.5)*(e_M1+ePrev_M1)+integralPrev_M1;
   double integral_M2 = (ki_M2*T*0.5)*(e_M2+ePrev_M2)+integralPrev_M2;
 
-  /*
-  //reset integral term if motor position moves from above to below from target or vice versa
-  if ((e_M1 > 0 & integralPrev_M1 < 0) | (e_M1 < 0 & integralPrev_M1 > 0)){
-    integralPrev_M1 = 0.0;
-  } 
-
-  //integral clamping
-  if (integral_M1 > 255){
-    integral_M1 = 255.0;
-  } else if (integral_M1 < -255){
-    integral_M1 = -255.0;
-  }
-
-  if (integral_M2 > 255){
-    integral_M2 = 255.0;
-  } else if (integral_M2 < -255){
-    integral_M2 = -255.0;
-  }
-  */
-
   //Compute derivative term
-  double eDerivative_M1 = (e_M1-(ePrev_M1))/(deltaT);
-  double eDerivative_M2 = (e_M2-(ePrev_M2))/(deltaT);
+  double derivative_M1 = kd_M1*((e_M1-(ePrev_M1))/(T));
+  double derivative_M2 = kd_M2*((e_M2-(ePrev_M2))/(T));
 
   //update variables for next iteration
-  prevTime = currentTime;
+  //prevTime = currentTime;
   ePrev_M1 = e_M1;
   ePrev_M2 = e_M2;
   integralPrev_M1 = integral_M1;
   integralPrev_M2 = integral_M2;
 
-  *u_M1 = (proportional_M1) + (integral_M1) + (kd_M1*eDerivative_M1);
-  *u_M2 = (proportional_M2) + (integral_M2) + (kd_M2*eDerivative_M2);
+  *u_M1 = (proportional_M1) + (integral_M1) + (derivative_M1);
+  *u_M2 = (proportional_M2) + (integral_M2) + (derivative_M2);
 }
 
 void moveMotor(int in1, int in2, int pwm_pin, double u){
@@ -186,6 +172,62 @@ void moveMotor(int in1, int in2, int pwm_pin, double u){
 }
 
 void loop() {
+  //#define START = digitalRead(start_pin);
+  //#define RESET = digitalRead(reset_pin);
+
+  switch(state){
+    case HOME:
+      //if(START) state = DRAW_1;
+      target_M1 = 0;
+      target_M2 = 0;
+
+      if((pos_M1 >= -5 && pos_M1 <= 5) && (pos_M2 >= -5 && pos_M2 <= 5)){
+        delay(1000);
+        integralPrev_M1 = 0.0;
+        integralPrev_M2 = 0.0;
+        state = DRAW_1;
+      }
+      break;
+    case DRAW_1:
+      //if(RESET) state = HOME;
+      target_M1 = 90;
+      target_M2 = 0;
+
+      //go to next state if; +/- 5 degree margin of error
+      if((pos_M1 >= 85 && pos_M1 <= 95) && (pos_M2 >= -5 && pos_M2 <= 5)){
+        delay(1000);
+        integralPrev_M1 = 0.0;
+        integralPrev_M2 = 0.0;
+        state = DRAW_2;
+      }
+      break;
+    case DRAW_2:
+      //if(RESET) state = HOME;
+      target_M1 = 90;
+      target_M2 = 90;
+
+      //go to next state if; +/- 5 degree margin of error
+      if((pos_M1 >= 85 && pos_M1 <= 95) && (pos_M2 >= 85 && pos_M2 <= 95)){
+        delay(1000);
+        integralPrev_M1 = 0.0;
+        integralPrev_M2 = 0.0;
+        state = DRAW_3;
+      }
+      break;
+    case DRAW_3:
+      //if(RESET) state = HOME;
+      target_M1 = 0;
+      target_M2 = 90;
+
+      //go to next state if; +/- 5 degree margin of error
+      if((pos_M1 >= -5 && pos_M1 <= 5) && (pos_M2 >= 85 && pos_M2 <= 95)){
+        delay(1000);
+        integralPrev_M1 = 0.0;
+        integralPrev_M2 = 0.0;
+        state = HOME;
+      }
+      break;
+  }
   
   Serial.print("target_M1:");
   Serial.print(target_M1);
@@ -195,7 +237,6 @@ void loop() {
   Serial.print(" | ");
   Serial.print("controlSignal_M1:");
   Serial.print(controlSignal_M1);
-
   Serial.print(" | ");
   Serial.print("target_M2:");
   Serial.print(target_M2);
@@ -206,10 +247,10 @@ void loop() {
   Serial.print("controlSignal_M2:");
   Serial.println(controlSignal_M2);
   
-
   /*
   Serial.print("end_time:");
   Serial.print(end_time);
   Serial.println("us");
   */
+
 }
